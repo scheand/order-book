2354a02a407c2327a63aefc56fc0665165ec4ccc
scheand <andrii.scherbinin@gmail.com>
2018-03-17 12:01:51 +0200

    initialize maven project

---
+++ src/test/java/org/and/orderbook/AppTest.java
@@ -0,0 +1,13 @@
+package org.and.orderbook;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void testApp() {
+        Assertions.assertTrue(true);
+    }
+}
